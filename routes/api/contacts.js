4cd0ab499222b9739c35342ddc759ee0a8d4f2bb
Bartek Bachara <bbachara@poczta.onet.pl>
2024-07-10 01:23:22 +0200

    hw04-auth push 3
    1. Removed middleware/passport.js to keep only auth.js file.
    2. Corrected index.js to contacts.js
    3. Corrected login and logout in user.js
    4. Moved logic from server.js to app.js

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const ctrlContact = require("../../controller/index");
+const ctrlContact = require("../../controller/contacts");
 const auth = require("../../middleware/auth");
 
 router.get("/", auth, ctrlContact.get);
